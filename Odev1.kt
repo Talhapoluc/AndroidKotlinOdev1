80b82807662180289a2925a8523d87be47bfda22
Talhapoluc <113974373+Talhapoluc@users.noreply.github.com>
2022-09-28 12:27:25 +0300

    Odev1
    Android Kotlin Eğitimi ödev-1

---
+++ Odev1.kt
@@ -0,0 +1,24 @@
+package com.talhapoluc.bootcampkotlinlearning.degisken_olusturma
+
+fun main() {
+    val islemNo = 1
+    val ogrNo = 1
+    val kitapNo = 1
+    val aTarih = "01.01.2022"
+    val vTarih = "01.01.2022"
+    val ogrAd = "Talha"
+    val ogrSoyad = "Poluç"
+    val cinsiyet = "Erkek"
+    val dTarih = "12.12.1997"
+    val sinif = "1/a"
+    val isbnNo = "1"
+    val kitapAdi = "Beyaz Zambaklar Ülkesinde"
+    val yazarNo = "1"
+    val turNo = "1"
+    val sayfaSayisi = "1"
+    val puan = "1"
+    val yazarAd = "Grigoriy"
+    val yazarSoyad = "Petrov"
+    val turAdi = "Kurgusal Olmayan"
+
+}
